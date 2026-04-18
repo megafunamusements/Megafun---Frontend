d2f2002312ce901c87072514f7261420470eda63
ArshadParvez <Muhammedarshadk46@gmail.com>
2026-04-19 03:54:39 +0530

    fix netlify config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,18 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'megafun-api.onrender.com' },
       { protocol: 'https', hostname: 'productimages.withfloats.com' },
       { protocol: 'https', hostname: 'fpimages.withfloats.com' },
       { protocol: 'https', hostname: 'bizimages.withfloats.com' },
       { protocol: 'https', hostname: 'backgroundimages.withfloats.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'megafun-api.onrender.com' },
-      { protocol: 'https', hostname: '*.onrender.com' },
       { protocol: 'http', hostname: 'localhost', port: '8000' },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
